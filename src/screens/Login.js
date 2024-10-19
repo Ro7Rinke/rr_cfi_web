0ccec605c3fda702edb37a591a6b4ae63a738f54
Rocetti Rinke <ro7rinke@gmail.com>
2024-10-19 18:20:40 -0300

    alterado layout de login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -2,10 +2,10 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import API from '../api/rr_cfi_api';
 import { useDispatch } from 'react-redux';
-import { setAuthToken } from '../redux/actions/authTokenActions'
+import { setAuthToken } from '../redux/actions/authTokenActions';
 
 const Login = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
@@ -13,12 +13,12 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const result = await API.getAuthToken(username, password)
-      if(result.access){
-        dispatch(setAuthToken(result))
-        navigate('/home')
-      }else{
-        alert("Usuário ou senha inválidos")
+      const result = await API.getAuthToken(username, password);
+      if (result.access) {
+        dispatch(setAuthToken(result));
+        navigate('/home');
+      } else {
+        alert("Usuário ou senha inválidos");
       }
     } catch (error) {
       alert('Falha ao efetuar o login');
@@ -27,27 +27,29 @@ const Login = () => {
 
   return (
     <div style={styles.container}>
-      <h2>Login</h2>
+      <h2>RR_CFI</h2>
       <form onSubmit={handleSubmit} style={styles.form}>
-        <div>
+        <div style={styles.inputGroup}>
           <label>Username</label>
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
+            style={styles.input}
           />
         </div>
-        <div>
+        <div style={styles.inputGroup}>
           <label>Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
+            style={styles.input}
           />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" style={styles.button}>Login</button>
       </form>
     </div>
   );
@@ -60,12 +62,37 @@ const styles = {
     alignItems: 'center',
     justifyContent: 'center',
     height: '100vh',
+    padding: '0 20px',
+    boxSizing: 'border-box',
   },
   form: {
     display: 'flex',
     flexDirection: 'column',
-    width: '300px',
-  }
+    width: '100%',
+    maxWidth: '400px',
+  },
+  inputGroup: {
+    marginBottom: '15px',
+  },
+  input: {
+    width: '100%',
+    padding: '10px',
+    borderRadius: '4px',
+    border: '1px solid #ccc',
+    fontSize: '16px',
+    boxSizing: 'border-box', // Inclui padding e bordas na largura total
+  },
+  button: {
+    width: '100%',
+    padding: '10px',
+    borderRadius: '4px',
+    border: 'none',
+    backgroundColor: '#007bff',
+    color: '#fff',
+    fontSize: '16px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+  },
 };
 
 export default Login;
