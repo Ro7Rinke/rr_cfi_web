e136286396ffa2587aaa03667db0e80ebc639790
Rocetti Rinke <ro7rinke@gmail.com>
2024-10-22 18:37:30 -0300

    Removido importação não usada

--- src/components/ProtectedRouter.jsx
+++ src/components/ProtectedRouter.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useLocation, Navigate } from 'react-router-dom';
 
